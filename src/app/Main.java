7b11ae07845b41bc0567435c2fb9d9978d40d4a0
Romain Rabouan <romain.rabouan1@gmail.com>
2020-12-11 22:31:48 +0100

    Mac icon

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,17 +8,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 
+import javax.swing.*;
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("InstaLike");
+
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("1024.png")));
+
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -55,6 +62,13 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        try {
+            URL iconURL = Main.class.getResource("1024.png");
+            java.awt.Image image = new ImageIcon(iconURL).getImage();
+            com.apple.eawt.Application.getApplication().setDockIconImage(image);
+        } catch (Exception e) {
+            // Ne marche pas sur Windows et Linux
+        }
         launch(args);
     }
 }
