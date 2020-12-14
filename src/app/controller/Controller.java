c937dad80839a7c8fef3a8ce5353688c1418f198
Romain Rabouan <romain.rabouan1@gmail.com>
2020-12-14 12:38:10 +0100

    Image logo

--- src/app/controller/Controller.java
+++ src/app/controller/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.regex.Pattern;
 
 
 public class Controller {
@@ -21,13 +22,24 @@ public class Controller {
 
     @FXML
     public void handleSignUp(ActionEvent event) {
-        System.out.println("Handle sign up with " + userNameTextField.getText() + " and " + passwordTextField.getText());
-        // Send to backend and get response
+
+        if (!handleCheckPasswordOnSignUp(passwordTextField.getText())) {
+            System.out.println("Le mot de passe doit contenir au moins 6 caractères, une majuscule et un chiffre.");
+            return;
+        }
+
+        System.out.println("Handle sign up succeed with " + userNameTextField.getText() + " and " + passwordTextField.getText());
+
+        // Ouvrir autre scene
+
+        // Send to backend and get response.
+
     }
 
-    @FXML
-    public void handleForgotPassword(ActionEvent event) {
-        System.out.println("Handle password forgotten");
-        // Send to backend and get response
+    private Boolean handleCheckPasswordOnSignUp(String pw) {
+        if(pw.isEmpty() || pw.length() < 6 || !Pattern.compile("[0-9]").matcher(pw).find() || !Helpers.checkString(pw)){
+            return false;
+        }
+        return true;
     }
 }
