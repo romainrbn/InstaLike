3caa7e6c5a0d75ade31f6e014d111b6063e37e25
Romain Rabouan <romain.rabouan1@gmail.com>
2021-01-23 09:51:44 +0100

    Image ronde

--- src/app/controller/CommentsViewController.java
+++ src/app/controller/CommentsViewController.java
@@ -1,4 +1,19 @@
 package app.controller;
 
-public class CommentsViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CommentsViewController implements Initializable {
+
+    @FXML
+    private ImageView profileImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Helpers.maskRoundImage(profileImageView);
+    }
 }
